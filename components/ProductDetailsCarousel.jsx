dd3800e1b98528616eefdf59da512a60552fd8ff
Alvin Albert Michael <113367440+alvin-dotcom@users.noreply.github.com>
2024-07-17 15:04:18 +0530

    Update ProductDetailsCarousel.jsx

--- components/ProductDetailsCarousel.jsx
+++ components/ProductDetailsCarousel.jsx
@@ -19,12 +19,12 @@ const ProductDetailsCarousel = ({ images }) => {
                         alt={img.attributes.name}
                     />
                 ))}
-                {/* <img src="/p2.png" />
+                <img src="/p2.png" />
                 <img src="/p3.png" />
                 <img src="/p4.png" />
                 <img src="/p5.png" />
                 <img src="/p6.png" />
-                <img src="/p7.png" /> */}
+                <img src="/p7.png" />
             </Carousel>
         </div>
     );
